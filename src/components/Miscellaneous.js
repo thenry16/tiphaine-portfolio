646dfdca3ec21e23b457e256de1c9c000133611e
TIPH TIPH <tiphaine.henry02@orange.fr>
2018-08-29 13:22:30 -0700

    arrow back

--- src/components/Miscellaneous.js
+++ src/components/Miscellaneous.js
@@ -13,12 +13,12 @@ import ContentWrapper from '../components/article/contentwrapper'
 const ArrowWrapper = styled.div`
   display: none;
   position: relative;
-  top: 5px;
+  top: -5px;
   @media (min-width: 500px) {
     display: block;
   }
   @media (min-width: 768px) {
-    top: 5px;
+    top: -5px;
   }
   width: 100%;
 `
